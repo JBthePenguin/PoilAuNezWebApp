086249448ae45e86427742407ccb8b30c8150ccf
JBthePenguin <jblab976@gmail.com>
2019-01-19 11:39:10 +0100

    add tests for actu

--- using/actu/tests.py
+++ using/actu/tests.py
@@ -1,5 +1,8 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class BrowseActuTests(StaticLiveServerTestCase):
@@ -23,3 +26,46 @@ class BrowseActuTests(StaticLiveServerTestCase):
         # header
         header_title = self.selenium.find_element_by_tag_name("h1")
         self.assertEqual(header_title.text, "Actualités")
+        last_actu_title = self.selenium.find_element_by_css_selector(
+            "#header_actus .card .card-body .card-title"
+        )
+        self.assertEqual(last_actu_title.text, "tests pagination 4")
+        # main
+        actu_display_links = self.selenium.find_elements_by_css_selector(
+            "#main_actus .card-text a"
+        )
+        actu_display_links[1].click()
+        wait = WebDriverWait(self.selenium, 10)
+        wait.until(
+            EC.presence_of_element_located(
+                (
+                    By.XPATH,
+                    "//img[@class='card-img-top' and @src='/uploads/fixtures/actus/alapoursuite.png']"
+                )
+            )
+        )
+        display_actu_title = self.selenium.find_element_by_css_selector(
+            "#header_actus .card .card-body .card-title"
+        )
+        self.assertEqual(display_actu_title.text, "tests pagination 3")
+        # pagination
+        actu_cards = self.selenium.find_elements_by_css_selector(
+            "#main_actus .card"
+        )
+        self.assertEqual(len(actu_cards), 4)
+        next_button = self.selenium.find_element_by_partial_link_text(
+            "Suivant")
+        next_button.click()
+        wait = WebDriverWait(self.selenium, 10)
+        wait.until(
+            EC.presence_of_element_located(
+                (
+                    By.XPATH,
+                    "//img[@class='w-100' and @src='/uploads/fixtures/actus/clownference.png']"
+                )
+            )
+        )
+        actu_cards = self.selenium.find_elements_by_css_selector(
+            "#main_actus .card"
+        )
+        self.assertEqual(len(actu_cards), 2)
