638d8e8d214f1e169fc453e62fdd3c845c753336
JBthePenguin <jblab976@gmail.com>
2019-01-20 12:01:45 +0100

    add test for photoapp

--- usingapp/galeryapp/photoapp/tests.py
+++ usingapp/galeryapp/photoapp/tests.py
@@ -1,3 +1,55 @@
-from django.test import TestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-# Create your tests here.
+
+class BrowsePhotoTests(StaticLiveServerTestCase):
+    """ Tests for the browsing"""
+    fixtures = ['photo_fixtures.json']
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = WebDriver()
+        cls.selenium.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def test_photos(self):
+        """ test for photos page"""
+        self.selenium.get('%s%s' % (self.live_server_url, '/galery/photos/'))
+        # header
+        header_title = self.selenium.find_element_by_tag_name("h1")
+        self.assertEqual(header_title.text, "Photos")
+        photos = self.selenium.find_elements_by_css_selector(
+            "#header_photos  .carousel-item"
+        )
+        self.assertEqual(len(photos), 6)
+        # main part and display
+        photos_titles = self.selenium.find_elements_by_css_selector(
+            "#main_photos  .card-body h6"
+        )
+        self.assertEqual(photos_titles[0].text, "Festiviel 2018 Test")
+        # pagination
+        self.assertEqual(len(photos_titles), 6)
+        next_button = self.selenium.find_element_by_partial_link_text(
+            "Suivant")
+        next_button.click()
+        wait = WebDriverWait(self.selenium, 10)
+        wait.until(
+            EC.presence_of_element_located(
+                (
+                    By.XPATH,
+                    "//img[@src='/uploads/test/pag/test.png']"
+                )
+            )
+        )
+        photos_cards = self.selenium.find_elements_by_css_selector(
+            "#main_photos .card"
+        )
+        self.assertEqual(len(photos_cards), 4)
